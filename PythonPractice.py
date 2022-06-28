8331e5ef6ca78d8456aebe47f33794c665337211
hsd9681 <hsd9681@naver.com>
2022-06-28 20:49:00 +0900

    밴드명생성해보기

--- PythonPractice.py
+++ PythonPractice.py
@@ -1,1 +1,14 @@
+#오늘 배운 내용
 print("hello")
+print("hello\nfriends")
+print("hello" + "buddy")# 두 단어가 붙어서 나옴
+#공백 넣기
+print("hello " + "buddy")
+print("hello" + " buddy")
+print("hello" +" "+ "buddy")
+
+#1일차 프로젝트 밴드명 생성
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
